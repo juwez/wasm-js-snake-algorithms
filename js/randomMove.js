e750104dd0d5956c85bfc675bc9329a5e311c280
juwez <joeylybaert@outlook.com>
2022-05-02 12:41:38 +0200

    randomMove finished

--- js/randomMove.js
+++ js/randomMove.js
@@ -14,8 +14,6 @@ let snake;
 const startXSnake = Math.floor(ROWS / 2);
 const startYSnake = Math.floor(COLS / 2);
 
-let food_x
-let food_y
 let direction;
 let snakeAlive=false;
 let foodOnBoard=false;
@@ -29,27 +27,26 @@ function respawnSnake() {
 }
 function moveSnake(){
   let moves=[]
-  
-  if (snake[0].x !== ROWS - 1) {moves.push('canMoveRight')}
-  if (snake[0].x !== 0) {moves.push('canMoveLeft')}
-
-  if (snake[0].y !== COLS - 1) {moves.push('canMoveUp')}
-  if (snake[0].y !== 0 ) {moves.push('canMoveDown')}
-  console.log(snake[0].y)
-
-
+  // check boundries
+  if (snake[0].x !== ROWS - 1  && !snake.some(e => e.x === snake[0].x + 1 && e.y === snake[0].y)) {moves.push('canMoveRight')}
+  if (snake[0].x !== 0 && !snake.some(e => e.x === snake[0].x - 1 && e.y === snake[0].y)) {moves.push('canMoveLeft')}
+  if (snake[0].y !== COLS - 1 && !snake.some(e => e.x === snake[0].x && e.y === snake[0].y + 1)) {moves.push('canMoveDown')}
+  if (
+    snake[0].y !== 0 &&
+   !snake.some(e => e.x === snake[0].x + 1 && e.y === snake[0].y - 1)) {
+    moves.push("canMoveUp");
+  }
   moves = moves.filter(Boolean)
   if(moves.length === 0)
   
   {snakeAlive = false}
   let direction = Math.floor(Math.random() * moves.length);
-  console.log(direction)
   let move = moves[direction];
-  console.log(moves)
-  console.log(move)
+
   if(move ==='canMoveRight'){
+
       snake.unshift({ x: snake[0].x + 1, y: snake[0].y });
-      if (!(snake[0].x + 1 === food_x && snake[0].y === food_y)) {
+      if (!(snake[0].x === food.x && snake[0].y === food.y)) {
         snake.pop();
       } else {
         eatFood();
@@ -58,24 +55,24 @@ function moveSnake(){
 else if(move ==='canMoveLeft'){
     
       snake.unshift({ x: snake[0].x - 1, y: snake[0].y });
-      if (!(snake[0].x - 1 === food_x && snake[0].y === food_y)) {
+      if (!(snake[0].x === food.x && snake[0].y === food.y)) {
         snake.pop();
       } else {
         eatFood();
       }
     }
     
-    else if(move ==='canMoveUp'){
+    else if(move ==='canMoveDown'){
       snake.unshift({ x: snake[0].x, y: snake[0].y + 1 });
-      if (!(snake[0].x === food_x && snake[0].y + 1 === food_y)) {
+      if (!(snake[0].x === food.x && snake[0].y === food.y)) {
         snake.pop();
       } else {
         eatFood();
       }
     }
-    else if (move === "canMoveDown") {
+    else if (move === "canMoveUp") {
       snake.unshift({ x: snake[0].x, y: snake[0].y - 1 });
-      if (!(snake[0].x - 1 === food_x && snake[0].y - 1 === food_y)) {
+      if (!(snake[0].x === food.x && snake[0].y === food.y)) {
         snake.pop();
       } else {
         eatFood();
@@ -91,8 +88,7 @@ else if(move ==='canMoveLeft'){
 
 function placeFood() {
   if (!foodOnBoard){
-    food_x = Math.floor(Math.random() * size);
-    food_y = Math.floor(Math.random() * size);
+    food={x:Math.floor(Math.random() * size),y:Math.floor(Math.random() * size)}
     foodOnBoard = true 
   }
 }
@@ -100,35 +96,47 @@ function eatFood(){
   score += 10;
   foodOnBoard = false;
 }
-
-function draw() {
-  respawnSnake()
-  placeFood()
-  moveSnake();
-  ctx.clear
+function drawBoard() {
     for (let x = 0; x < COLS; ++x) {
       for (let y = 0; y < ROWS; ++y) {
-        if (y == food_y && x == food_x) {
-          ctx.fillStyle = "red";
-        }
-         else {
           ctx.strokeStyle = "green";
           ctx.lineWidth = "0.8";
           ctx.fillStyle = "black";
-          
-        }
+        
         ctx.fillRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W, BLOCK_H);
         ctx.strokeRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W, BLOCK_H);
       }
-       }
-    for (let i = 0; i < snake.length; i++) {
-         ctx.fillStyle = "white";
-         ctx.fillRect(BLOCK_W * snake[i].x, BLOCK_H * snake[i].y, BLOCK_W, BLOCK_H);
+    }
+  }
+function drawFood(){
+    ctx.fillStyle = "red";
+    ctx.fillRect(BLOCK_W * food.x, BLOCK_H * food.y, BLOCK_W, BLOCK_H);
+    ctx.strokeRect(BLOCK_W * food.x, BLOCK_H * food.y, BLOCK_W, BLOCK_H);
+  }
+function drawSnake() {
+  //drawSnakeHead
+ctx.fillStyle = "yellow";
+ctx.fillRect(BLOCK_W * snake[0].x, BLOCK_H * snake[0].y, BLOCK_W, BLOCK_H);
+ctx.strokeRect(BLOCK_W * snake[0].x, BLOCK_H * snake[0].y, BLOCK_W, BLOCK_H);
+//drawSnake
+ctx.fillStyle = "white";
+snake.slice(1).forEach(element => {
+ctx.fillRect(BLOCK_W * element.x, BLOCK_H * element.y, BLOCK_W, BLOCK_H);
+ctx.strokeRect(BLOCK_W * element.x, BLOCK_H * element.y, BLOCK_W, BLOCK_H);
+});
 }
+function draw() {
+  respawnSnake()
+  placeFood()
+  moveSnake()
+  drawBoard()
+  drawFood()
+  drawSnake()
+  
 }
 
 function startGame() {
-    setInterval(draw, 50);
+    setInterval(draw, 3000);
   }
 
 startGame();
